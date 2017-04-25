83f8da636dbe7fdf3e3ea41b51d85a7098417a32
Nvertigo <nvertigo67@gmail.com>
2017-04-25 15:22:25 +0200

    fix update script.

--- tools/update.sh
+++ tools/update.sh
@@ -2,5 +2,5 @@
 
 SOURCE=${1-/mnt/export/data/src/oxygenos}
 
-cp -av ${SOURCE}/firmware-update firmware-update
-cp -av ${SOURCE}/RADIO RADIO
+cp -av ${SOURCE}/firmware-update/* firmware-update
+cp -av ${SOURCE}/RADIO/* RADIO
